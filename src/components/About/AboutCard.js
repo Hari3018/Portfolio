cde53d2340d75c3f9d8563b6fca644eb2c69165d
HARI PRASAD S <harirsmani2003@gmail.com>
2025-11-02 00:16:50 +0530

    final submit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <Card className="quote-card-view">
       <Card.Body>
@@ -11,22 +13,16 @@ function AboutCard() {
             Hi Everyone, I am <span className="purple">Hari Prasad S </span>
             from <span className="purple"> TamilNadu, India.</span>
             <br />
-            I am currently persuing undergraduate degree in Computer Science and Engineering Departmnet at CIT.
+            I am currently pursuing an undergraduate degree in the Computer Science and Engineering Department at CIT.
             <br />
             I have completed my secondary school in Bharathi Matric Higher Secondary School.
-            Mesra.
             <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            I am a dedicated student aspiring to become a software developer. I have a strong passion for coding, problem-solving, and building efficient software solutions. Along the way, I am also exploring data analysis to enhance my ability to work with structured information and derive meaningful insights.
+
+            With a keen interest in developing scalable applications, I am continuously learning and improving my skills in programming, algorithms, and system design. My goal is to grow as a developer, contribute to impactful projects, and create innovative solutions that make a difference.
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+          
+          
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
@@ -34,6 +30,8 @@ function AboutCard() {
           <footer className="blockquote-footer">Hari Prasad S</footer>
         </blockquote>
       </Card.Body>
+
+     
     </Card>
   );
 }
